0a714cc022a3a42163869d1e809e15a446193c5e
Andrea Palladino <andreagpalladino@gmail.com>
2020-07-24 13:48:48 +0200

    spostato tutto su Home e HomeController, stile navbar

--- resources/views/components/_navbar.blade.php
+++ resources/views/components/_navbar.blade.php
@@ -1,39 +1,39 @@
 <nav class="navbar navbar-expand-lg bg-transparent shadow fixed-top">
     <div class="container">
-      <a class="navbar-brand text-warning" href="{{route('homepage')}}">theBooker</a>
+      <a class="navbar-brand text-warning" href="{{route('home')}}">theBooker</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <a class="nav-link text-white" href="{{route('homepage')}}">Home
+            <a class="nav-link navlink navlink:hover" href="{{route('home')}}">Home
                   <span class="sr-only">(current)</span>
                 </a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link text-white" href="#">About</a>
+            <a class="nav-link navlink navlink:hover" href="#">Who</a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link text-white" href="#">Services</a>
+            <a class="nav-link navlink navlink:hover" href="#">Books</a>
           </li>
           <li class="nav-item active">
-            <a class="nav-link text-white" href="#">Contact</a>
+            <a class="nav-link navlink navlink:hover" href="#">Contact</a>
           </li>
           
             <!-- Authentication Links -->
             @guest
                 <li class="nav-item active">
-                    <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    <a class="nav-link text-warning text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
                 </li>
                 @if (Route::has('register'))
                     <li class="nav-item active">
-                        <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        <a class="nav-link text-warning text-white" href="{{ route('register') }}">{{ __('Register') }}</a>
                     </li>
                 @endif
             @else
                 <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    <a id="navbarDropdown" class="nav-link text-warning dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }} <span class="caret"></span>
                     </a>
 
@@ -47,8 +47,12 @@
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                             @csrf
                         </form>
+                        <a class="dropdown-item" href="">Add a Book</a>
+                        
                     </div>
+                    
                 </li>
+                
             @endguest
     
         </ul>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+<header id="sfondo" class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center">
+        <div class="col-12 text-center">
+          <h1 class="class mb-1"><sup>the</sup>Booker</h1>
+          <hr id="bar">
+          <p id="subtitle" class="lead">a great way to share readings</p>
+          <button class="btn-warning my-5" id="btn" value="Turn-on the Lights">Turn-on the Lights</button>
+        </div>
+      </div>
+    </div>
+  </header>
+@endsection
+
+
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -19,5 +35,4 @@
             </div>
         </div>
     </div>
-</div>
-@endsection
+</div> --}}
